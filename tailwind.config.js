e2d913ad49571b2b9a982e82912728ad7f988569
M Asad Munawar <112193656+adm1n1strat0r-MAM@users.noreply.github.com>
2024-04-03 23:12:49 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'white' : '#FBF9FA',
+      'red' : '#FF0000',
+      'black' : '#2B2024'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
